3b7acacb8b778b3d358a808d7f36cdc6bbbfb22c
luansantiago0 <luansantiago0200@gmail.com>
2023-08-18 19:08:28 -0300

    alteracao-intro

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval(function () {
-  nextImage();
-}, 5000);
-
-function nextImage() {
-  count++;
-  if (count > 3) {
-    count = 1;
-  }
-
-  document.getElementById("radio" + count).checked = true;
-}
+const menu = document.querySelector(".menu");
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
